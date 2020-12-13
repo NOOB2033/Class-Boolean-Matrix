325deab496c2417b211b3059f0f250d5bb100d9c
MaxonTheSlayer <zaza32228@mail.ru>
2020-12-13 12:31:34 +0700

    w

--- Class Boolean Matrix/BoolVector.cpp
+++ Class Boolean Matrix/BoolVector.cpp
@@ -349,6 +349,8 @@ BoolVector& BoolVector::operator|=(const BoolVector& other)
 	int min = mem_;
 	if (mem_ > other.mem_)
 		min = other.mem_;
+	if (len_ < other.len_)
+		len_ = other.len_;
 	for (int i = 0; i < min; i++)
 		bv_[mem_ - i - 1] |= other.bv_[other.mem_ - i - 1];
 	return *this;
@@ -366,6 +368,8 @@ BoolVector& BoolVector::operator&=(const BoolVector& other)
 	int min = mem_;
 	if (mem_ > other.mem_)
 		min = other.mem_;
+	if (len_ < other.len_)
+		len_ = other.len_;
 	for (int i = 0; i < min; i++)
 		bv_[mem_ - i - 1] &= other.bv_[other.mem_ - i - 1];
 	return *this;
@@ -383,6 +387,8 @@ BoolVector& BoolVector::operator^=(const BoolVector& other)
 	int min = mem_;
 	if (mem_ > other.mem_)
 		min = other.mem_;
+	if (len_ < other.len_)
+		len_ = other.len_;
 	for (int i = 0; i < min; i++)
 		bv_[mem_ - i - 1] ^= other.bv_[other.mem_ - i - 1];
 	return *this;
--- Class Boolean Matrix/main.cpp
+++ Class Boolean Matrix/main.cpp
@@ -1,53 +1,12 @@
 #include<iostream>
 #include"Enum.h"
 #include"BoolMatrix.h"
-#include"BoolVector.h"
-
-
-int* sortMatrix(BoolMatrix bm, BoolVector bv, int size)
-{
-	int flag = 0;
-	int index = 0;
-	int* a = new int[size];
-	for (; bv.weight();) {                                         // until the array is empty
-		if (flag == size) {
-			std::cout << "There is a cycle in the graph" << std::endl;
-			for (int i = 0; i < size; i++)
-				a[i] = -1;
-			return a;
-		}
-		for (int column = 0; column < size; column++)
-			if (bv.getBit(column)) {                             // if the column is not removed from the set
-				int count = 0;
-				for (int line = 0; line < size & !count; line++) // !count(no 1 in column)
-					if (bm[line].getBit(column))
-						count++;
-				if (!count) {                                    // if column has no 1
-					bm.set0(0, size, column);                    // set0 line (column == line)
-					for (int i = 0; i < size; i++)               // set0 column
-						bm[i].set0(column);
-					bv.set0(column);                             // set0 element of the set
-					a[index++] = column;
-				}
-			}
-		flag++;
-	}
-	return a;
-}
 
 
 int main()
 {
 	try
 	{
-		int size;
-		std::cin >> size;
-		BoolMatrix bm(size, size, 0);
-		std::cin >> bm;
-		BoolVector bv(size, 1);
-		int* a = sortMatrix(bm, bv, size);
-		for (int i = 0; i < size; i++)
-			std::cout << a[i] << " ";
 	}
 	catch (Mistakes mistake)
 	{
